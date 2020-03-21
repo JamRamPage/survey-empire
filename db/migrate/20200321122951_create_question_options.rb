71f70fb103ea20d9cc10e2364948db9310638139
James Page <jp00965@surrey.ac.uk>
2020-03-21 12:37:24 +0000

    Added Scaffold allowing users to provide options for a question (multiple-choice). Answers can be correct/wrong or neither (in the case of opinion-based surveys)

---
+++ db/migrate/20200321122951_create_question_options.rb
@@ -0,0 +1,11 @@
+class CreateQuestionOptions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :question_options do |t|
+      t.belongs_to :question, foreign_key: true, null: false
+      t.string :optionString, null: false
+      t.boolean :correct, comment: 'can be neither, if the survey is subjective'
+
+      t.timestamps
+    end
+  end
+end
