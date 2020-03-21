97180ebebca5f65c3c35a43f2d66a7193626a3f3
James Page <jp00965@surrey.ac.uk>
2020-03-21 12:11:44 +0000

    Linked survey table to user table - Surveys are created by a logged in user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_123956) do
+ActiveRecord::Schema.define(version: 2020_03_21_115950) do
 
   create_table "questions", force: :cascade do |t|
     t.integer "survey_id"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_03_19_123956) do
     t.integer "dislikes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_surveys_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
