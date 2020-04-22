52e3b60d0ee901bf30637579cb05d64b6d13f6ba
James Page <jp00965@surrey.ac.uk>
2020-04-22 17:25:04 +0100

    Modified Survey show.html.erb page to show each and every answer and selection for all questions within a survey - this will be the data analytics page, showing appropriate graphs

--- app/models/question_option_selection.rb
+++ app/models/question_option_selection.rb
@@ -1,4 +1,10 @@
 class QuestionOptionSelection < ApplicationRecord
   belongs_to :question_answer
   belongs_to :question_option
+  def text
+    QuestionOption.find_by(id: self.question_option_id).optionString
+  end
+  def correct
+    QuestionOption.find_by(id: self.question_option_id).correct
+  end
 end
