0a6e6854968c7038ff4e95c604d517e78ded282f
Stefano Bussandri <articpanda43@gmail.com>
2020-05-18 12:21:08 +0100

    Added searching only to search table

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', function() {
-    $('#survey-table').DataTable({
-      bJQueryUI: true,
-      sPaginationType: "full_numbers",
-      iDisplayLength: 100,
-      "searching": true,
+    oTable = $('#survey-table').DataTable({
+      "orderable": true,
+      info: false,
+      "dom":'<<"row toolbar-row"<"col toolbar"f>><"spacer"><"row toolbar-row"<"col entries"l>><"half-spacer">t<"half-spacer"><"row toolbar-row"<"col"p>>>',
+      language: { search: "", searchPlaceholder: "Survey Title" },
     });
-    $('.dataTables_length').addClass('bs-select');
+    $('div.dataTables_filter input').addClass('border rounded shadow-sm d-inline searchbar');
 });
